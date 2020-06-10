27d7d71091090eff293ebf2f07cb21b97e4f665f
Rudolf <kovalevskiy2017@outlook.com>
2020-06-10 17:10:43 +0300

    'Menu'

---
+++ js/javascript.js
@@ -0,0 +1,8 @@
+window.onload = function (){
+
+document.querySelector('#out').innerHTML = '<h1>Hello Rudolf!!!!!</h1>';
+
+document.getElementById('hider').onclick = function () {
+    document.getElementById('hide').style.display = 'none';
+}
+};
