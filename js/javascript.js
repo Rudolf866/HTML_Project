afca30a2e936a2ae61a6b2f91daffb5566ea7fbd
Rudolf <kovalevskiy2017@outlook.com>
2020-06-11 12:53:54 +0300

    'projects'

--- js/javascript.js
+++ js/javascript.js
@@ -1,8 +1,5 @@
 window.onload = function (){
-
-document.querySelector('#out').innerHTML = '<h1>Hello Rudolf!!!!!</h1>';
-
-document.getElementById('hider').onclick = function () {
-    document.getElementById('hide').style.display = 'none';
+     document.getElementById('hider').onclick = function () {
+     document.getElementById('hide').style.display = 'none';
 }
 };
